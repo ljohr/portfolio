9c5f66e09a66ec7771ccfb048c2a835d3af2b2f2
Lilly Ohr <lillyohr@gmail.com>
2024-06-08 10:48:41 -0500

    Add mouse zoom and pan

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 import Loader from "../components/Loader";
 
 import Macintosh from "../models/Macintosh.jsx";
+import Table from "../models/Table.jsx";
 
 const Home = () => {
   const adjustMacForScreen = () => {
     let screenScale = [4, 4, 4];
-    let screenPosition = [0, 0, 0];
+    let screenPosition = [0, 0.5, 0];
     let macRotation = [0.45, 3.15, 0];
 
     // if (window.innerWidth < 768) {
@@ -23,7 +25,6 @@ const Home = () => {
 
   return (
     <section className="homeSection">
-      <h1>Home</h1>
       <Canvas camera={{ near: 0.1, far: 1000, position: [0, 0, 10] }}>
         <Suspense fallback={<Loader />}>
           <directionalLight position={[3, 1, 1]} intensity={2} />
@@ -33,6 +34,12 @@ const Home = () => {
             position={macPosition}
             scale={macScale}
             rotation={macRotation}
+          />
+          <OrbitControls
+            enableZoom={true}
+            minDistance={8}
+            maxDistance={20}
+            enablePan={true}
           />
         </Suspense>
       </Canvas>
